9f5288ce38bd95235ab55bdca8c2d23c044709f7
Htet-Myat-Soe <htetmyatsoe492@gmail.com>
2022-01-09 17:54:55 +0630

    sec commit

--- main.js
+++ main.js
@@ -1,60 +1,72 @@
-const display = document.querySelector(".res-text");
-const button = document.querySelector(".generate");
-const range = document.querySelector("#range");
-const num = document.querySelector("#number");
-const char = document.querySelector("#special-char");
-const copy = document.querySelector(".copy");
+// DOM  Select
 
-const strongest = `qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890!@#$%^&*()<>,.?/||:;`;
-const strong = `qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()<>,.?/||:;`;
+const display = document.querySelector(".res-text");
+const copy = document.querySelector(".copy");
+const range = document.querySelector("#range");
+const rangeRes = document.querySelector(".range-res");
+const numInput = document.querySelector("#number");
+const charInput = document.querySelector("#special-char");
+const button = document.querySelector(".generate");
+
+const strongest = `qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()<>?{}[]1234567890`;
+const strong = `qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()<>?{}[]`;
 const middle = `qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890`;
 const weak = `qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM`;
 
 button.addEventListener("click",generatePw);
 
+
 function generatePw(){
     let length = range.value;
-    let numCheck = num.checked;
-    let charCheck = char.checked;
+    let numCheck = numInput.checked;
+    let charCheck = charInput.checked;
 
-    let genPw = '';
+    let newPw = '';
 
-    if(numCheck && charCheck){
-        for(let i = 0; i < length; i++){
-            genPw += strongest[Math.floor(Math.random() * strongest.length)];
+   if(numCheck && charCheck){
+        for(let i = 0; i < length;i++){
+            newPw += strongest[Math.floor(Math.random() * strongest.length)];
         }
+   }
+   else if(numCheck){
+    for(let i = 0; i < length;i++){
+        newPw += middle[Math.floor(Math.random() * middle.length)];
     }
-    else if(numCheck){
-        for(let i = 0; i < length; i++){
-            genPw += middle[Math.floor(Math.random() * middle.length)];
-        }
+   }
+   else if(charCheck){
+    for(let i = 0; i < length;i++){
+        newPw += strong[Math.floor(Math.random() * strong.length)];
     }
-    else if(charCheck){
-        for(let i = 0; i < length; i++){
-            genPw += strong[Math.floor(Math.random() * strong.length)];
-        }
+   }
+   else{
+    for(let i = 0; i < length;i++){
+        newPw += weak[Math.floor(Math.random() * weak.length)];
     }
-    else{
-        for(let i = 0; i < length; i++){
-            genPw += weak[Math.floor(Math.random() * weak.length)];
-        }
-    }
-    
+   }
 
-    display.value = genPw;
+   display.value = newPw;
+
+
+   
 }
 
+
+
+// Range Value
+
 range.addEventListener("change",() => {
-    document.querySelector(".range-res").innerHTML = range.value;
+    rangeRes.innerHTML = range.value;
 });
 
+
+// Copy
+
 copy.addEventListener("click",() => {
-    display.select();
-    display.setSelectionRange(0,9999);
+    if(display.value){
+        display.select();
+        display.setSelectionRange(0,99999);
 
-    navigator.clipboard.writeText(display.value);
-
-    alert("Copy Success");
-
-    display.value = '';
+        navigator.clipboard.writeText(display.value);
+        alert("Copy Success!");
+    }
 })
